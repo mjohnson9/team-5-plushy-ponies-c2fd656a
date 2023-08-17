15859826f37dc0060fa56460cb84a83566a5af33
Serge <shchavialeu.sa@gmail.com>
2023-08-17 19:15:05 +0000

    Added tests & functionality for GameMap

--- src/main/java/com/levelup/forestsandmonsters/game/GameMap.java
+++ src/main/java/com/levelup/forestsandmonsters/game/GameMap.java
@@ -1,10 +1,43 @@
 package com.levelup.forestsandmonsters.game;
 
+import java.awt.Point;
+
+import com.levelup.forestsandmonsters.game.GameController.DIRECTION;
+
 public class GameMap {
     int numPositions = 100;
 
+
     public int getTotalPositions() {
         return numPositions;
+    }
+
+    public Position[][] getPositions() {
+        Position[][] positions = new Position[0][0];
+        return positions;
+    }
+
+    public Position calculatedPosition(final Position startingPosition, DIRECTION direction) {
+        Position calculatedPosition = new Position(startingPosition.point.x, startingPosition.point.y);
+        if(direction.equals(DIRECTION.NORTH)){
+            calculatedPosition.point.y--;
+        } else if (direction.equals(DIRECTION.SOUTH)){
+            calculatedPosition.point.y++;
+        } else if (direction.equals(DIRECTION.EAST)){
+            calculatedPosition.point.x++;
+        } else if (direction.equals(DIRECTION.WEST)){
+            calculatedPosition.point.x--;
+        }
+        
+        if(isPositionValid(calculatedPosition.point)){
+            return calculatedPosition;
+        }
+        return startingPosition;
+    }
+
+    public boolean isPositionValid(Point positionCoordinates){
+        return ((positionCoordinates.x <= 9 && positionCoordinates.x>=0) 
+        && (positionCoordinates.y <= 9 && positionCoordinates.y>=0));
     }
 
 }
--- src/test/java/com/levelup/forestsandmonsters/game/GameMapTest.java
+++ src/test/java/com/levelup/forestsandmonsters/game/GameMapTest.java
@@ -1,11 +1,16 @@
 package com.levelup.forestsandmonsters.game;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.awt.Point;
 
 import org.junit.Test;
 
 import com.levelup.forestsandmonsters.game.GameMap;
+import com.levelup.forestsandmonsters.game.GameController.DIRECTION;
 
 public class GameMapTest {
     @Test
@@ -18,5 +23,32 @@ public class GameMapTest {
         GameMap testObj = new GameMap();
         int numPositions = testObj.getTotalPositions();
         assertEquals(100,numPositions);
+    }
+
+    @Test
+    public void getPostionsTest(){
+        GameMap testObj = new GameMap();
+        Position[][] returnedPositions = testObj.getPositions();
+        assertNotNull("Returned Positions are not null", returnedPositions);
+    }
+
+    @Test 
+    public void calculatePositionTest(){
+        GameMap testObj = new GameMap();
+        Position startingPosition = new Position(5, 5);
+        Position calculatedPosition = testObj.calculatedPosition(startingPosition, DIRECTION.NORTH);
+        Position expectedFinalPosition = new Position(5,4);
+        
+        System.out.println("sp: " + startingPosition.point + "cp: " + calculatedPosition.point);
+        assertNotNull("Returned calculated position is not null", calculatedPosition);
+        assertEquals(expectedFinalPosition.point, calculatedPosition.point);
+    }
+
+    @Test
+    public void isPositionValidTest(){
+        GameMap testObj = new GameMap();
+        assertTrue("This is valid", testObj.isPositionValid(new Point(5, 5)));
+        assertFalse("This is NOT valid", testObj.isPositionValid(new Point(10, 5)));
+        assertFalse("This is NOT valid", testObj.isPositionValid(new Point(5, 10)));
     }
 }
